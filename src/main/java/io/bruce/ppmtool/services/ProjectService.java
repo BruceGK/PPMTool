536123dd6dcc89c88088ae5ac91353a6a1ac9960
Chenhao Huang <huang98@terpmail.umd.edu>
2020-05-31 23:58:05 -0400

    expections

--- src/main/java/io/bruce/ppmtool/services/ProjectService.java
+++ src/main/java/io/bruce/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.bruce.ppmtool.services;
 
 import io.bruce.ppmtool.domain.Project;
+import io.bruce.ppmtool.exceptions.ProjectIdException;
 import io.bruce.ppmtool.repositories.ProjectRepository;
 import org.bruce.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,11 +12,14 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project saveOrUpdateProject(Project project){
+    public Project saveOrUpdateProject(Project project) {
 
-        //Logic
-
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
     }
 
 }
